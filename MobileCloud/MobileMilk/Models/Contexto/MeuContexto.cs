bb8ff5bf9fe01bd1a27fe8576d9c1575458bbd7d
1613719 <renanvieiraleite@gmail.com>
2017-11-06 17:17:28 -0200

    Projeto Finalizado

---
+++ MobileCloud/MobileMilk/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MobileMilk.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Web.Models.MobileMilk.Models.Item> Items { get; set; }
+    }
+}
---
+++ MobileCloud/MobileMilk/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Models
+{
+    namespace MobileMilk.Models
+    {
+        public class Item
+        {
+            public int ItemID { get; set; }
+            public string Nome { get; set; }
+            //colocar para ter acento:
+            public string Descricao { get; set; }
+            public string Sexo { get; set; }
+        }
+    }
+}
